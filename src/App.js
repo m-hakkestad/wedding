73c9b4da9e83c5258cf08e5558fde0cd80015868
Mårten <m.hakkestad@gmail.com>
2018-12-07 23:05:38 +0100

    basic page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
@@ -9,8 +8,16 @@ class App extends Component {
 
         <header className="App-header">
             <div className="App-header-top">
+                <div className="circle red"></div>
+                <div className="circle orange"></div>
+                <div className="circle yellow"></div>
+                <div className="circle green"></div>
+                <div className="circle blue"></div>
+                <div className="circle purple"></div>
+                <div className="circle pink"></div>
             </div>
             <div className="App-header-bottom">
+                <h1>M & M</h1>
             </div>
         </header>
 
