cd398edf9a2c8db0d9c3236f1afb6290a17d3c84
Mårten <m.hakkestad@gmail.com>
2018-12-08 00:01:52 +0100

    Updates

---
+++ precache-manifest.69d1c1431d0c714a876b7a1a6b1a5c13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0ad16e246cac35125e3",
+    "url": "/wedding/static/css/main.83be75f5.chunk.css"
+  },
+  {
+    "revision": "e0ad16e246cac35125e3",
+    "url": "/wedding/static/js/main.e0ad16e2.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/wedding/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "0e44ce2450fbf11c30d5",
+    "url": "/wedding/static/js/runtime~main.0e44ce24.js"
+  },
+  {
+    "revision": "3b4a0d4d2ff1be6dcf2f99137569318c",
+    "url": "/wedding/index.html"
+  }
+];
