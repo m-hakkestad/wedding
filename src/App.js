b1dcb59d09908a89e6eda14ab48c03400df3aade
Mårten <m.hakkestad@gmail.com>
2018-12-07 17:51:37 +0100

    init commit added react

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <header className="App-header">
+            <div className="App-header-top">
+            </div>
+            <div className="App-header-bottom">
+            </div>
+        </header>
+
+        <div className="App-body">
+        </div>
+
+        <footer className="App-footer">
+        </footer>
+
+      </div>
+    );
+  }
+}
+
+export default App;
